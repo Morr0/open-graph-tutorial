310153b47fd8f7f92ce2321959b6476ffab11ae6
Rami Hadad <rami@ramihikmat.net>
2021-07-25 15:32:28 +1000

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+
+function App() {
+  const [previews, setPreviews] = useState([]);
+  const [currentUrl, setCurrentUrl] = useState('');
+  const [loadingPreview, setLoadingPreview] = useState(false);
+
+  const addClick = (e) => {
+    e.preventDefault();
+
+    setLoadingPreview(true);
+    fetch(`http://localhost:3500/meta?url=${encodeURIComponent(currentUrl)}`)
+    .catch((error) => alert(`Error. ${error}`))
+    .then(async (response) => {
+      const preview = await response.json();
+      setPreviews(x => {
+        return [
+          ...x,
+          preview
+        ];
+      });
+      setLoadingPreview(false);
+    });
+
+  };
+
+  return (
+    <div>
+      <header>
+        <input type="url" value={currentUrl} onChange={(e) => setCurrentUrl(e.target.value)} placeholder="Enter URL" />
+        <button type="button" onClick={addClick} disabled={loadingPreview}>Preview</button>
+      </header>
+      <main>
+        <ul>
+          {previews.map((preview, index) => (
+            <li key={index} style={{
+              display: 'flex',
+              flexDirection: 'column',
+              borderBottom: '1px solid black'
+            }}>
+              <p>Site: {preview.site_name}</p>
+              <p>Title: {preview.title}</p>
+              <p>Description: {preview.description}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
